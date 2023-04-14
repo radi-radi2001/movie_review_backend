9b0643ef14489ff3b677d2a563b331bea5abd991
RadoslavKirilov <36498160+radi-radi2001@users.noreply.github.com>
2023-04-14 22:33:30 +0100

    Add JsonIgnore to add properly movie or tvshow with review and returning everything

--- src/main/java/com/example/SpringDemo/pojos/TvShow.java
+++ src/main/java/com/example/SpringDemo/pojos/TvShow.java
@@ -11,7 +11,7 @@ import java.util.Date;
 public class TvShow {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy =GenerationType.AUTO)
     @Column(name = "tvshowId")
     private int tvShowId;
 
